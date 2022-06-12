c8e4392b4eef1b9e6d89b276888bc24f18cb8453
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-06-12 15:04:16 -0700

    Add WildWestRP and PRC

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -14,7 +14,9 @@ export const regOthers = [
 	{ name: 'Sundown RP', reg: /\bsundown\s*(?:rp\b|roleplay)/i, include: 1 },
 	{ name: 'NewCenturyRP', reg: /\bnc\s*rp|\bnewcentury\s*(?:rp\b|roleplay)/i, include: 1 },
 	{ name: 'New Valley RP', reg: /\bnew\s*valley\s*(?:rp\b|roleplay)/i, include: 1 },
+	{ name: 'Wild West RP', reg: /\bww\s*rp|\bwild\s*west\s*(?:rp\b|roleplay)/i, include: 1 },
 	{ name: 'Haven\'s Crest RP', reg: /\bhaven'?s\s+crest\s+(?:rp\b|roleplay)/i, include: 1 },
+	{ name: 'PRC', reg: /\bprc\b/i, include: 1 },
 	{ name: 'Red Dead Online', reg: /\b(?:rdr[\s:-]*[2]?|read\s*dead)[\s:-]*online/i, include: 0 },
 	{ name: 'RDR Story', reg: /story[\s\-]*mode|\brdr[\s:-]*[2]?[\s\-]+story/i, include: 0 },
 ];
