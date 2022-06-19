2a95b3a9340f2061c95e7b8c7682077b76fafc2a
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-06-18 18:40:44 -0700

    Add ts interface for Twitch's embedded video js

---
+++ frontend/src/twitch-embed/Player.d.ts
@@ -0,0 +1,26 @@
+export interface Player {
+    disableCaptions(): void;
+    enableCaptions(): void;
+    pause(): void;
+    play(): void;
+    seek(timestamp: number): void;
+    setChannel(channel: string): void;
+    setCollection(collectionId: string, videoId?: string): void;
+    setQuality(quality: string): void;
+    setVideo(videoId: String, timestamp: string): void;
+
+    getMuted(): boolean;
+    setMuted(muted: boolean): void;
+    getVolume(): number;
+    setVolume(volume: number): void;
+
+    getPlaybackStats(): PlaybackStats;
+    getChannel(): string | undefined;
+    getCurrentTime(): number | undefined;
+    getDuration(): number | undefined;
+    getEnded(): boolean;
+    getQualities(): string[];
+    getQuality(): string;
+    getVideo(): string | undefined;
+    isPaused(): boolean;
+}
