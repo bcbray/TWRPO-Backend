a6167ee87e3ff86c997fc8f4e88e3b44bb182d5f
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-06-20 22:45:55 -0700

    Nicer 404 page. Kinda.

--- frontend/src/NotFound.tsx
+++ frontend/src/NotFound.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Helmet } from "react-helmet-async";
 
 const NotFound: React.FunctionComponent<{}> = () => {
   return (
-    <p>Not found.</p>
+    <>
+      <Helmet>
+        <title>Twitch WildRP Only - Not Found</title>
+      </Helmet>
+      <Container className="mt-5">
+        <h3>Not found.</h3>
+        <p>Couldn't find the page you were looking for. Sorry.</p>
+      </Container>
+    </>
   );
 };
 
