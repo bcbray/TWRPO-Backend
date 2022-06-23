918598ea671af840aa25fe4ec688602ae8799f4f
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-06-23 03:16:30 -0500

    2x profile photos where appropriate

--- frontend/src/ProfilePhoto.tsx
+++ frontend/src/ProfilePhoto.tsx
@@ -12,16 +12,18 @@ interface ProfilePhotoProps {
   size?: ProfilePhotoSize;
 }
 
-const resizedProfileUrl = (url: string, size: number) => {
+const resizedProfileUrl = (url: string, size: number, scale: number = 1) => {
   // Twitch profile photo sizes, via https://github.com/justintv/Twitch-API/issues/28#issuecomment-11549604
   // 600x600, 300x300, 150x150, 70x70, 50x50, 28x28
 
-  const size =
-    size <= 28 ? '28x28'
-    : size <= 50 ? '50x50'
-    : size <= 70 ? '70x70'
-    : size <= 150 ? '150x150'
-    : size <= 300 ? '300x300'
+  const useSize = size * scale;
+
+  const urlSize =
+    useSize <= 28 ? '28x28'
+    : useSize <= 50 ? '50x50'
+    : useSize <= 70 ? '70x70'
+    : useSize <= 150 ? '150x150'
+    : useSize <= 300 ? '300x300'
     : '600x600';
 
   const result = url.replace(/(profile_image-)(\d+x\d+)(\.\w+)$/, (_, prefix, __, suffix) => {
@@ -38,6 +40,8 @@ export const ProfilePhoto = React.forwardRef<
 >((
   { channelInfo, size = 'sm', className, loading = 'lazy', style, ...rest }, ref
 ) => {
+  const scale = useDevicePixelRatio();
+
   const sizePx =
     size === 'sm' ? 30
     : size === 'md' ? 60
@@ -63,7 +67,7 @@ export const ProfilePhoto = React.forwardRef<
     >
       {channelInfo &&
         <img
-          src={resizedProfileUrl(channelInfo.profilePictureUrl, sizePx)}
+          src={resizedProfileUrl(channelInfo.profilePictureUrl, sizePx, scale)}
           alt={channelInfo.displayName}
           loading={loading}
         />
--- frontend/src/hooks.tsx
+++ frontend/src/hooks.tsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react';
 import { useSearchParams, NavigateOptions  } from 'react-router-dom';
 
+const isBrowser = typeof window !== 'undefined';
 
 export const useSingleSearchParam = (name: string): [string, ((value: string, options?: NavigateOptions) => void)] => {
   const [params, setParams] = useSearchParams();
@@ -16,3 +18,22 @@ export const useSingleSearchParam = (name: string): [string, ((value: string, op
   const param = params.get(name) ?? '';
   return [param, setParam];
 };
+
+export const useDevicePixelRatio = (fallback: number = 1) => {
+  const [ratio, setRatio] = useState(isBrowser ? window.devicePixelRatio : fallback);
+
+  useEffect(() => {
+    if (isBrowser) {
+      const handler = () => setRatio(window.devicePixelRatio);
+
+      // ref: https://bugs.chromium.org/p/chromium/issues/detail?id=123694
+      const media = window.matchMedia('screen and (min-resolution: 1.5)');
+      media.addEventListener('change', handler);
+      return () => {
+        media.removeEventListener('change', handler);
+      }
+    }
+  }, []);
+
+  return ratio;
+}
