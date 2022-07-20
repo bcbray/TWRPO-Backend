9671acbfe1313e1fe2390dbedc08190a69f4f81a
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-07-19 17:46:05 -0700

    Update medical and law regexes

--- src/data/factions.ts
+++ src/data/factions.ts
@@ -24,10 +24,10 @@ export const wrpFactionsRegex = {
     podcast: noLater(noFormer(/\b(?:(?<!!)podcast|rp\s+theory)\b/i)),
     watchparty: noLater(noFormer(/\b(?<!!)watch[\s\-_.]*part/i)),
     development: /\bdevelop|\bdev\b|\bcoding|devathon/i,
-    law: noLater(noFormer(/\b(?:deputy|cadet|detective|sheriff)\b/i)),
+    law: noLater(noFormer(/\b(?:deputy|cadet|detective|sheriff|scsd)\b/i)),
     rangers: noFormer(/\brangers?\b/i),
     dicenzofamiglia: noFormer(/\bdicenzos?\b/i),
-    medical: noLater(/(?<!then\b.*|!)(?:doctor|\b[teplch]-\d{1,2}\b|paramedic|therapist|psychologist|\b(?:dr(?!\s*pepper)|em[st])\b)/i),
+    medical: noLater(/(?<!then\b.*|!)(?:doctor|medic|paramedic|therapist|psychologist|\b(?:dr(?!\s*pepper)|em[st]|scdh)\b)/i),
     onelife: /\bperma?thon|\bperma\s*character|\b(?:one|1)[\s\-_.]*life/i,
 } as { [key in WrpFactionsRegexKeys]: RegExp };
 
