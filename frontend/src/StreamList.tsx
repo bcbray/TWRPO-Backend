2770dc4973ae34a8bacd99eac59ef87de252e498
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-07-21 21:39:11 -0700

    Animate reordering on Live view

--- frontend/src/StreamList.tsx
+++ frontend/src/StreamList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col} from 'react-bootstrap';
+import { Flipper, Flipped } from 'react-flip-toolkit';
 
 import styles from './StreamList.module.css';
 import { Stream, CharacterInfo, FactionInfo } from './types';
@@ -16,26 +17,30 @@ interface Props {
 const StreamList: React.FC<Props> = ({ streams, offlineCharacters, factionInfos, loadTick }) => {
   return (
     <>
-    <Row className={styles.row}>
-      {streams.map(stream => (
-        <Col key={stream.channelName} xs={12} sm={6} md={4} lg={3}>
-          <StreamCard
-            stream={stream}
-            factionInfos={factionInfos}
-            loadTick={loadTick}
-          />
-        </Col>
-      ))}
-      {offlineCharacters && offlineCharacters.map(character => (
-        <Col key={`${character.channelName}_${character.name}`} xs={12} sm={6} md={4} lg={3}>
-          <OfflineCharacterCard
-            className={styles.offline}
-            character={character}
-            factionInfos={factionInfos}
-          />
-        </Col>
-      ))}
-    </Row>
+    <Flipper flipKey={loadTick}>
+      <Row className={styles.row}>
+        {streams.map(stream => (
+          <Flipped key={stream.channelName} flipId={stream.channelName}>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <StreamCard
+                stream={stream}
+                factionInfos={factionInfos}
+                loadTick={loadTick}
+              />
+            </Col>
+          </Flipped>
+        ))}
+        {offlineCharacters && offlineCharacters.map(character => (
+          <Col key={`${character.channelName}_${character.name}`} xs={12} sm={6} md={4} lg={3}>
+            <OfflineCharacterCard
+              className={styles.offline}
+              character={character}
+              factionInfos={factionInfos}
+            />
+          </Col>
+        ))}
+      </Row>
+    </Flipper>
     </>
   );
 };
