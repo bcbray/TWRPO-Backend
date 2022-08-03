c34968ebcb80e13db7c6ddf782f6b3be9479c5d4
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-08-03 01:01:25 -0700

    Fix offline character cards in dark mode

--- frontend/src/OfflineCharacterCard.tsx
+++ frontend/src/OfflineCharacterCard.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import styles from './OfflineCharacterCard.module.css';
 import { CharacterInfo, FactionInfo } from './types';
+import { classes } from './utils';
+import { useFactionCss, factionStylesForKey } from './hooks';
 import Tag from './Tag';
 import ProfilePhoto from './ProfilePhoto';
 import OutboundLink from './OutboundLink';
@@ -36,47 +38,49 @@ const CharacterLink: React.FC<CharacterLinkProps> = ({character, style, children
 );
 
 const OfflineCharacterCard = React.forwardRef<HTMLDivElement, Props>((
-  { character, factionInfos, className, ...rest }, ref
-) => (
-  <div className={[styles.card, className].join(' ')} ref={ref} {...rest}>
-    <div className={styles.thumbnail}>
-      <CharacterLink character={character}>
-        <span>Offline</span>
-      </CharacterLink>
-      <Tag
-        className={[styles.tag, styles.name].join(' ')}
-        style={{
-          background: character.factions[0]?.colorDark,
-        }}
-      >
-        <p>{character.displayInfo.displayName}</p>
-      </Tag>
-    </div>
-    <div className={[styles.info, 'stream-card-info'].join(' ')}>
-      <ProfilePhoto
-        className={styles.pfp}
-        channelInfo={character.channelInfo}
-        size={30}
-      />
-      <div className={styles.text}>
-        <div className={styles.title}>
-          <p title={character.name}>{character.name}</p>
-        </div>
-        <div className={styles.channel}>
-          <p>
-            <CharacterLink
-              character={character}
-              style={{
-                color: character.factions[0]?.colorLight
-              }}
-            >
-              {character.channelName}
-            </CharacterLink>
-          </p>
+  { character, factionInfos, className, style, ...rest }, ref
+) => {
+  const factionContainer = useFactionCss(Object.values(factionInfos));
+
+  return (
+    <div
+      className={classes(styles.card, className, factionContainer)}
+      ref={ref}
+      style={{
+        ...factionStylesForKey(character.factions[0]?.key),
+        ...style
+      }}
+      {...rest}
+    >
+      <div className={styles.thumbnail}>
+        <CharacterLink character={character}>
+          <span>Offline</span>
+        </CharacterLink>
+        <Tag className={classes(styles.tag, styles.name)}>
+          <p>{character.displayInfo.displayName}</p>
+        </Tag>
+      </div>
+      <div className={classes(styles.info, 'stream-card-info')}>
+        <ProfilePhoto
+          className={styles.pfp}
+          channelInfo={character.channelInfo}
+          size={30}
+        />
+        <div className={styles.text}>
+          <div className={styles.title}>
+            <p title={character.name}>{character.name}</p>
+          </div>
+          <div className={styles.channel}>
+            <p>
+              <CharacterLink character={character}>
+                {character.channelName}
+              </CharacterLink>
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-));
+  );
+});
 
 export default OfflineCharacterCard;
