6eb4098f16003f6db09ad7141c66ca3f01f1c6ef
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-08-10 17:11:45 -0700

    301 for HTTPS redirect

--- frontend/server/requireHttps.ts
+++ frontend/server/requireHttps.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from 'express';
 
 const requireHttps: RequestHandler = (req, res, next) => {
   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
-    return res.redirect(`https://${req.get('host')}${req.url}`);
+    return res.redirect(`https://${req.get('host')}${req.url}`, 301);
   }
   next();
 };
