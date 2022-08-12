6f8c00370a2869d0fc5bf8ee5df60e8f40246568
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-08-12 07:18:34 -0700

    Reference entities by type not path
    These get webpacked, ya dummy

--- packages/api/src/db/dataSource.ts
+++ packages/api/src/db/dataSource.ts
@@ -1,12 +1,14 @@
 import path from 'path';
 import { DataSource } from 'typeorm';
 
+import { StreamChunk } from './entity/StreamChunk';
+
 export default function dataSource(postgresUrl: string): DataSource {
     return new DataSource({
         type: 'postgres',
         url: postgresUrl,
         ssl: { rejectUnauthorized: false },
-        entities: [path.resolve(__dirname, 'entity/*.ts')],
+        entities: [StreamChunk],
         migrations: [path.resolve(__dirname, 'migration/*.ts')],
         subscribers: [],
     });
