06b45322cbbc036f5d969a1ab8ef30f405952fd6
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-08-18 22:58:47 -0700

    Don't HTTPS-redirect localhost, no matter the env

--- frontend/server/requireHttps.ts
+++ frontend/server/requireHttps.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from 'express';
 
 const requireHttps: RequestHandler = (req, res, next) => {
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development' && req.hostname !== 'localhost') {
     return res.redirect(`https://${req.get('host')}${req.url}`, 301);
   }
   next();
