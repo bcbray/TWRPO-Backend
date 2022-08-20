24dcd34f089a64e174a89e1a49d62827ccf1d07c
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-08-20 00:25:11 -0700

    Add “initial” migration
    Rather than an inconsistent “what's in the database because of migrations vs synchronization”
    question constantly, always use migrations to bootstrap a fresh database

--- packages/api/scripts/bootstrap-db.ts
+++ packages/api/scripts/bootstrap-db.ts
@@ -3,16 +3,8 @@ import { StreamChunk } from '../src/db/entity/StreamChunk';
 
 async function bootstrap(): Promise<void> {
     const dataStore = await initializeDataStore();
-    const { tableName } = dataStore.getMetadata(StreamChunk);
-    const queryRunner = dataStore.createQueryRunner();
-    const hasTable = await queryRunner.hasTable(tableName);
-    if (!hasTable) {
-        console.log('Synchronizing');
-        await dataStore.synchronize();
-    } else {
-        console.info('Running migrations');
-        await dataStore.runMigrations();
-    }
+    console.info('Running migrations');
+    await dataStore.runMigrations();
 }
 
 bootstrap()
