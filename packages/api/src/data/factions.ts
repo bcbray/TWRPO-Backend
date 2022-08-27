ffd66e13781ad05e08a2db02bd51f559752e9920
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-08-27 12:56:26 -0700

    Fix permathon regex

--- packages/api/src/data/factions.ts
+++ packages/api/src/data/factions.ts
@@ -28,7 +28,7 @@ export const wrpFactionsRegex = {
     rangers: noFormer(/\brangers?\b/i),
     dicenzofamiglia: noFormer(/\bdicenzos?\b/i),
     medical: noLater(/(?<!then\b.*|!)(?:doctor|medic|paramedic|therapist|psychologist|\b(?:dr(?!(?:\s*pepper|\s+will\s+see\s+you))|em[st]|scdh)\b)/i),
-    onelife: /\bperma?thon|\bperma\s*character|\b(?:one|1)[\s\-_.]*life/i,
+    onelife: /\bperma-?thon|\bperma\s*character|\b(?:one|1)[\s\-_.]*life/i,
 } as { [key in WrpFactionsRegexKeys]: RegExp };
 
 export const wrpFactionsSubRegex: { [key in FactionRealMini]?: [string, RegExp][] } = {
