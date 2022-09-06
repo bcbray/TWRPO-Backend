44ff478782917b5c8f9841403db6edcadbdbdc36
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-09-06 14:58:00 -0700

    Add Henry Dahlman

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -23,7 +23,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 1026
+// Next ID: 1027
 
 export const wrpCharacters: WrpCharacters = {
     '0Reed': [
@@ -1958,6 +1958,7 @@ export const wrpCharacters: WrpCharacters = {
     ],
     uncledirtbag: [
         { id: 1025, name: 'Jason Maynard' },
+        { id: 1026, name: 'Henry Dahlman' },
     ],
     unstoppableLARN: [
         { id: 910, name: 'Gertrude Cockburn' },
