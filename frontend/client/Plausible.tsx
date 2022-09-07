3d0612b01aabe5a44b79db09941e9496adf82d42
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-09-07 03:05:22 -0700

    Better SSR for Plausible

--- frontend/client/Plausible.tsx
+++ frontend/client/Plausible.tsx
@@ -2,23 +2,30 @@ import React from 'react';
 import PlausibleTracker from 'plausible-tracker';
 import type { PlausibleOptions } from 'plausible-tracker';
 
-export type PlausibleStuff = ReturnType<typeof PlausibleTracker>;
-
 const PlausibleContext = React.createContext<ReturnType<typeof PlausibleTracker> | null>(null);
+
+const noopTracker: ReturnType<typeof PlausibleTracker> = {
+  trackEvent: () => {},
+  trackPageview: () => {},
+  enableAutoPageviews: () => () => {},
+  enableAutoOutboundTracking: () => () => {},
+};
 
 export const usePlausible = () => {
   const plausible = React.useContext(PlausibleContext);
-  return plausible ?? {
-    trackEvent: () => {},
-    trackPageview: () => {},
-    enableAutoPageviews: () => () => {},
-    enableAutoOutboundTracking: () => () => {},
-  };
+  return plausible ?? noopTracker;
 }
 
 export const Plausible: React.FC<React.PropsWithChildren<PlausibleOptions>> = ({children, ...options}) => {
+  const plausible = React.useMemo(() => {
+    const isBrowser = typeof window !== 'undefined';
+    if (isBrowser) {
+      return PlausibleTracker(options);
+    } else {
+      return noopTracker;
+    }
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const plausible = React.useMemo(() => PlausibleTracker(options), []);
+  }, []);
   React.useEffect(() => plausible.enableAutoPageviews(), [plausible]);
   React.useEffect(() => plausible.enableAutoOutboundTracking(), [plausible]);
   return <PlausibleContext.Provider value={plausible}>
