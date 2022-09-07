bd82c6538c11d961f138792539030a02203ae7ae
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-09-06 20:36:22 -0700

    Slightly better footer styles

--- frontend/client/Footer.tsx
+++ frontend/client/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import styles from './Footer.module.css';
+import { classes } from './utils';
 
 interface FooterProps {
 
@@ -8,7 +9,7 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = () => {
   return (
-    <div className={styles.footer}>
+    <div className={classes(styles.footer, 'inset')}>
       <p>Twitch WildRP Only is a fan-made site and has no affiliation with WildRP, Twitch, or the streamers listed here.</p>
     </div>
   );
