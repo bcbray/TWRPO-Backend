0f85401548f94a19f44ae8253d645b8c4a4f4628
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-09-09 00:29:15 -0700

    Factor out fetchVideosForUser

--- packages/api/src/fetchVideos.ts
+++ packages/api/src/fetchVideos.ts
@@ -5,6 +5,68 @@ import { StreamChunk } from './db/entity/StreamChunk';
 import { TwitchChannel } from './db/entity/TwitchChannel';
 import { Video } from './db/entity/Video';
 
+export const fetchVideosForUser = async (
+    apiClient: ApiClient,
+    dataSource: DataSource,
+    userId: string,
+    knownStreamIds: Set<string>
+): Promise<number> => {
+    let foundVideos = 0;
+    let pages = 10;
+    let after: HelixForwardPagination['after'];
+    try {
+        while (pages > 0) {
+            console.log(JSON.stringify({
+                level: 'info',
+                message: `Fetching videos page “${11 - pages}” for ${userId}`,
+                event: 'video-fetch',
+                userId,
+            }));
+            const videos = await apiClient.videos.getVideosByUser(userId, {
+                type: 'archive',
+                after,
+                limit: 100,
+            });
+            for (const video of videos.data) {
+                if (video.streamId && knownStreamIds.has(video.streamId)) {
+                    await dataSource.getRepository(Video).insert({
+                        videoId: video.id,
+                        streamId: video.streamId,
+                        streamerId: video.userId,
+                        streamCreatedDate: video.creationDate,
+                        streamPublishedDate: video.publishDate,
+                        url: video.url,
+                        thumbnailUrl: video.thumbnailUrl,
+                        title: video.title,
+                        duration: video.duration,
+                    });
+                    console.log(JSON.stringify({
+                        level: 'info',
+                        message: `Found video “${video.title}” for ${video.userDisplayName}`,
+                        event: 'video-found',
+                        channel: video.userDisplayName,
+                        title: video.title,
+                    }));
+                    foundVideos += 1;
+                }
+            }
+            if (!videos.cursor) {
+                break;
+            }
+            after = videos.cursor;
+            pages -= 1;
+        }
+    } catch (error) {
+        console.log(JSON.stringify({
+            level: 'warning',
+            message: `Failed to fetch page “${11 - pages}” for ${userId}`,
+            event: 'video-fetch-failed',
+            error,
+        }));
+    }
+    return foundVideos;
+};
+
 export const fetchVideos = async (
     apiClient: ApiClient,
     dataSource: DataSource
@@ -25,96 +87,44 @@ export const fetchVideos = async (
     const streamerIds = new Set(chunksWithoutVideos.map(c => c.streamerId));
     const toFetch = [...streamerIds];
 
-    if (toFetch.length === 0) {
+    if (toFetch.length > 0) {
+        console.log(JSON.stringify({
+            level: 'info',
+            message: `Fetching videos for ${toFetch.length} users`,
+            event: 'video-start',
+            userCount: toFetch.length,
+        }));
+
+        const fetchStart = process.hrtime.bigint();
+        let foundVideos = 0;
+
+        for (const userId of [...toFetch]) {
+            foundVideos += await fetchVideosForUser(apiClient, dataSource, userId, streamIds);
+        }
+        await dataSource.getRepository(TwitchChannel)
+            .createQueryBuilder('twitch_channel')
+            .update()
+            .set({ lastVideoCheck: checkTime })
+            .where('twitch_channel."twitchId" IN (:...twitchIds)', { twitchIds: toFetch })
+            .execute();
+
+        const fetchEnd = process.hrtime.bigint();
+
+        console.log(JSON.stringify({
+            level: 'info',
+            message: `Fetched videos for ${toFetch.length} users, found ${foundVideos} videos`,
+            event: 'video-end',
+            userCount: toFetch.length,
+            foundVideos,
+            totalTime: Number((fetchEnd - fetchStart) / BigInt(1e+6)),
+        }));
+    } else {
         console.log(JSON.stringify({
             level: 'info',
             message: 'No stream segments to fetch videos for',
             event: 'video-skip',
         }));
-        return;
     }
-
-    console.log(JSON.stringify({
-        level: 'info',
-        message: `Fetching videos for ${toFetch.length} users`,
-        event: 'video-start',
-        userCount: toFetch.length,
-    }));
-
-    const fetchStart = process.hrtime.bigint();
-    let foundVideos = 0;
-
-    for (const userId of [...toFetch]) {
-        let pages = 10;
-        let after: HelixForwardPagination['after'];
-        try {
-            while (pages > 0) {
-                console.log(JSON.stringify({
-                    level: 'info',
-                    message: `Fetching videos page “${11 - pages}” for ${userId}`,
-                    event: 'video-fetch',
-                    userId,
-                }));
-                const videos = await apiClient.videos.getVideosByUser(userId, {
-                    type: 'archive',
-                    after,
-                    limit: 100,
-                });
-                for (const video of videos.data) {
-                    if (video.streamId && streamIds.has(video.streamId)) {
-                        await dataSource.getRepository(Video).insert({
-                            videoId: video.id,
-                            streamId: video.streamId,
-                            streamerId: video.userId,
-                            streamCreatedDate: video.creationDate,
-                            streamPublishedDate: video.publishDate,
-                            url: video.url,
-                            thumbnailUrl: video.thumbnailUrl,
-                            title: video.title,
-                            duration: video.duration,
-                        });
-                        console.log(JSON.stringify({
-                            level: 'info',
-                            message: `Found video “${video.title}” for ${video.userDisplayName}`,
-                            event: 'video-found',
-                            channel: video.userDisplayName,
-                            title: video.title,
-                        }));
-                        foundVideos += 1;
-                    }
-                }
-                if (!videos.cursor) {
-                    break;
-                }
-                after = videos.cursor;
-                pages -= 1;
-            }
-        } catch (error) {
-            console.log(JSON.stringify({
-                level: 'warning',
-                message: `Failed to fetch page “${11 - pages}” for ${userId}`,
-                event: 'video-fetch',
-                error,
-            }));
-        }
-    }
-    await dataSource.getRepository(TwitchChannel)
-        .createQueryBuilder('twitch_channel')
-        .update()
-        .set({ lastVideoCheck: checkTime })
-        .where('twitch_channel."twitchId" IN (:...twitchIds)', { twitchIds: toFetch })
-        .execute();
-
-    const fetchEnd = process.hrtime.bigint();
-
-    console.log(JSON.stringify({
-        level: 'info',
-        message: `Fetched videos for ${toFetch.length} users, found ${foundVideos} videos`,
-        event: 'video-end',
-        userCount: toFetch.length,
-        foundVideos,
-        totalTime: Number((fetchEnd - fetchStart) / BigInt(1e+6)),
-    }));
 };
 
 export type IntervalTimeout = ReturnType<typeof setInterval>;
