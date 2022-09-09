2263a49dcb7f5b2cb73bafdc7619e5ae05c3d76a
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-09-08 23:33:32 -0700

    Add Stream entity

--- packages/api/scripts/migrations/index.ts
+++ packages/api/scripts/migrations/index.ts
@@ -2,10 +2,12 @@ import { Initial1660336359134 } from './1660336359134-Initial';
 import { nullCharacterIndex1660355289987 } from './1660355289987-null-character-index';
 import { AddTwitchChannel1660551290102 } from './1660551290102-AddTwitchChannel';
 import { FixUncertainCharacters1660563350573 } from './1660563350573-FixUncertainCharacters';
+import { Stream1662698374839 } from './1662698374839-Stream';
 
 export default [
     Initial1660336359134,
     nullCharacterIndex1660355289987,
     AddTwitchChannel1660551290102,
     FixUncertainCharacters1660563350573,
+    Stream1662698374839,
 ];
--- packages/api/src/db/dataSource.ts
+++ packages/api/src/db/dataSource.ts
@@ -2,12 +2,13 @@ import { DataSource } from 'typeorm';
 
 import { StreamChunk } from './entity/StreamChunk';
 import { TwitchChannel } from './entity/TwitchChannel';
+import { Stream } from './entity/Stream';
 
 export default function dataSource(postgresUrl: string): DataSource {
     return new DataSource({
         type: 'postgres',
         url: postgresUrl,
         ssl: { rejectUnauthorized: false },
-        entities: [StreamChunk, TwitchChannel],
+        entities: [StreamChunk, TwitchChannel, Stream],
     });
 }
