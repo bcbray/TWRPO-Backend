bee7707ec02b62d6e94fc709ce74a53d46ef3aba
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-09-11 15:00:16 -0700

    Fix CharactersTable

--- frontend/client/CharactersTable.tsx
+++ frontend/client/CharactersTable.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from "react-router-dom";
-import { CharacterInfo, Stream } from '@twrpo/types';
+import { CharacterInfo } from '@twrpo/types';
 
 import styles from './CharactersTable.module.css';
 import LiveBadge from './LiveBadge';
