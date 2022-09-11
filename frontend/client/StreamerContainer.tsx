05479bab0f9b67d2df29a836ffeee7fac047fcc7
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-09-11 15:10:45 -0700

    Add Streamer pages

---
+++ frontend/client/StreamerContainer.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useParams } from "react-router-dom";
+
+import { useStreamer } from './Data';
+import { isSuccess, isFailure, NotFoundError } from './LoadingState';
+import Streamer from './Streamer';
+import Error from './Error';
+import Loading from './Loading';
+import NotFound from './NotFound';
+
+interface StreamerContainerProps {
+
+}
+
+const StreamerContainer: React.FC<StreamerContainerProps> = () => {
+  const params = useParams();
+  const { streamerName } = params;
+  const [loadState, reload, loadTick] = useStreamer(streamerName ?? '');
+
+  return (
+    <div className="content">
+      {isSuccess(loadState)
+        ? <Streamer data={loadState.data} loadTick={loadTick} />
+        : isFailure(loadState)
+          ? loadState.error instanceof NotFoundError
+            ? <NotFound alreadyContent />
+            : <Error onTryAgain={reload} />
+          : <Loading />
+       }
+    </div>
+  );
+};
+
+export default StreamerContainer;
