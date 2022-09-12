357e32e59e4b9153b52bad3dd0e6f8c35452a7ca
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-09-11 18:50:29 -0700

    Andrew Kennedy is a cadet

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -2099,7 +2099,7 @@ export const wrpCharacters: WrpCharacters = {
     ],
     Xiceman: [
         { id: 947, name: 'Michael Bayo', factions: ['The Unhung'] },
-        { id: 948, name: 'Andrew Kennedy' },
+        { id: 948, name: '[Cadet] Andrew Kennedy', factions: ['Law'] },
     ],
     xlt588gaming: [
         { id: 949, name: 'Adam Garica' },
