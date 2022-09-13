7abeb6773f6b21d4fc6a55a91fb7967c4b750312
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-09-13 00:52:35 -0700

    Create twrpo instance in index rather than sharing it globally

--- frontend/server/index.tsx
+++ frontend/server/index.tsx
@@ -6,10 +6,33 @@ import './tracer';
 import express from 'express';
 import http from 'http';
 import compression from 'compression';
+import { ClientCredentialsAuthProvider } from '@twurple/auth';
+import { TWRPOApi } from '@twrpo/api';
 
 import server from './server';
-import twrpo from './twrpo';
 import requireHttps from './requireHttps';
+
+console.log('Creating new client credentials...');
+if (!process.env.TWITCH_CLIENT_ID) {
+    console.log('Missing TWITCH_CLIENT_ID');
+    process.exit(1);
+}
+if (!process.env.TWITCH_CLIENT_SECRET) {
+    console.log('Missing TWITCH_CLIENT_SECRET');
+    process.exit(1);
+}
+const twitchAuthProvider = new ClientCredentialsAuthProvider(
+  process.env.TWITCH_CLIENT_ID,
+  process.env.TWITCH_CLIENT_SECRET
+);
+
+if (!process.env.DATABASE_URL) {
+    console.log('Missing DATABASE_URL');
+    process.exit(1);
+}
+const postgresUrl = process.env.DATABASE_URL;
+
+const twrpo = new TWRPOApi({ twitchAuthProvider, postgresUrl });
 
 twrpo.initialize()
   .then(() => {
