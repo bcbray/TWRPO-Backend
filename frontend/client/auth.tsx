4769d1f5a9f80fcb0b07307dcd19a0a25a3a318d
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-09-13 18:39:49 -0700

    Add login to useAuth

--- frontend/client/auth.tsx
+++ frontend/client/auth.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
+import { useEffectOnce } from 'react-use';
 import { User } from '@twrpo/types';
+
+import styles from './auth.module.css';
 
 import { isSuccess } from './LoadingState';
 import { useCurrentUser } from './Data';
 
 export interface UserInfo {
   user?: User;
+  login: () => void;
   logout: () => void;
 }
 
 const UserContext = React.createContext<UserInfo | null>(null);
 
 export const UserProvider: React.FC<{ children: React.ReactElement }> = ({ children }) => {
+  const popup = React.useRef<Window | null>(null);
+
   const [loadingState, reload] = useCurrentUser({
     ignoreReloadFailures: false,
   });
+
+  const login = React.useCallback(() => {
+    if (popup.current) {
+      popup.current.close();
+    }
+    if (typeof window === 'undefined') {
+      return;
+    }
+    const top = window.top ?? window;
+    const width = 600;
+    const height = 800;
+    const x = top.outerWidth / 2 + top.screenX - ( width / 2);
+    const y = top.outerHeight / 2 + top.screenY - ( height / 2);
+    popup.current = window.open('/auth/twitch', '_blank', `width=${width}, height=${height}, top=${y}, left=${x}`)
+  }, []);
+
   const logout = React.useCallback(() => {
     fetch('/auth/logout', { method: 'POST' })
       .then(() => {
@@ -26,10 +48,36 @@ export const UserProvider: React.FC<{ children: React.ReactElement }> = ({ child
       })
   }, [reload]);
 
+  const receiveMessage = React.useCallback((event: MessageEvent<AuthPopupMessage>) => {
+    if (event.origin !== window.location.origin) {
+      return;
+    }
+    if (event.data.isa !== 'auth-message') {
+      return;
+    }
+    if (event.data.type === 'auth-complete') {
+      // TODO: Do something if auth failed?
+      reload();
+    } else if (event.data.type === 'auth-close') {
+      popup.current?.close();
+      popup.current = null;
+    }
+  }, [reload]);
+
+  React.useEffect(() => {
+    window.addEventListener('message', receiveMessage, true);
+    return () => {
+      window.removeEventListener('message', receiveMessage, true);
+    };
+  }, [receiveMessage]);
+
+  const user = isSuccess(loadingState) ? loadingState.data : undefined;
+
   const info: UserInfo = React.useMemo(() => ({
-    user: isSuccess(loadingState) ? loadingState.data : undefined,
+    user,
+    login,
     logout,
-  }), [loadingState, logout]);
+  }), [user, login, logout]);
 
   return <UserContext.Provider value={info}>
     {children}
@@ -40,12 +88,40 @@ export const useAuth = (): UserInfo => {
   const userInfo = React.useContext(UserContext);
   if (!userInfo) {
     console.error('Missing UserProvider');
-    return { logout: () => {} };
+    return {
+      login: () => {},
+      logout: () => {},
+    };
   }
   return userInfo;
 }
 
+interface AuthCompleteMessage {
+  isa: 'auth-message';
+  type: 'auth-complete';
+  success: boolean;
+}
+
+interface AuthCloseMessage {
+  isa: 'auth-message';
+  type: 'auth-close'
+}
+
+type AuthPopupMessage = AuthCompleteMessage | AuthCloseMessage;
+
 export const AuthComplete: React.FC<{ success: boolean }> = ({ success }) => {
+  useEffectOnce(() => {
+    if (typeof window ==='undefined' || !window.opener) {
+      return;
+    }
+    const completeMessage: AuthCompleteMessage = { isa: 'auth-message', type: 'auth-complete', success };
+    window.opener.postMessage(completeMessage, window.location.origin);
+    setTimeout(() => {
+      const closeMessage: AuthCloseMessage = { isa: 'auth-message', type: 'auth-close' };
+      window.opener.postMessage(closeMessage, window.location.origin);
+    }, 1500);
+  });
+
   return (
     <div className={styles.container}>
       {success ? (
