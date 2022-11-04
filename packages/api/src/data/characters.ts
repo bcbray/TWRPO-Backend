24bbd6d1328997daba91babe8fc89ad2d8a05c50
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-11-04 16:54:39 -0700

    Fix Big Bear

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -998,7 +998,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 989, name: 'Kaifeng Mi', factions: ['Independent', 'Guarma'], deceased: true },
         { id: 1054, name: 'Pocky Ma' },
         { id: 1173, name: 'Koxinga Zheng', nicknames: ['Mr. Ko', reg(/\bmr\.?\s+ko\b/)], displayName: 3 },
-        { id: 1223, name: 'Pretty Boy Stagley', displayName: 0 },
+        { id: 1223, name: 'Big Bear', nicknames: ['BigBear'], displayName: 0 },
     ],
     HerbtheNerd: [
         { id: 384, name: 'Charles Kane' },
