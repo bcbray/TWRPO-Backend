439f6cac96f452f12d87f64dd3fea2ada07a77d8
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-11-04 08:33:08 -0700

    Fix Speedy

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -937,7 +937,7 @@ export const wrpCharacters: WrpCharacters = {
     GooseyOfficial: [
         { id: 358, name: 'Jaimie Quinn' },
         { id: 359, name: 'MaN Sauers' },
-        { id: 360, name: 'Speedy McGillagully', factions: ['Dead End Kids'] },
+        { id: 360, name: 'Joseph "Speedy" McGillagully', factions: ['Dead End Kids'] },
         { id: 1015, name: 'Harold Roach' },
     ],
     GrammTheGibbon: [
