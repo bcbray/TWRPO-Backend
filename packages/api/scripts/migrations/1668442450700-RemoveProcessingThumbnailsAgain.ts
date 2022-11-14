a55208ea6dece373a872dcb2a9d5a1f6333138b6
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-11-14 08:18:14 -0800

    Fix migration for missing thumbnails more

--- packages/api/scripts/migrations/1668442450700-RemoveProcessingThumbnailsAgain.ts
+++ packages/api/scripts/migrations/1668442450700-RemoveProcessingThumbnailsAgain.ts
@@ -5,9 +5,34 @@ export class RemoveProcessingThumbnailsAgain1668442450700 implements MigrationIn
     name = 'RemoveProcessingThumbnailsAgain1668442450700'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        // Create a rollback point of the data we'll be removing
+        await queryRunner.query(`
+            CREATE TABLE video_1668442450700_removed_again AS
+            SELECT * FROM "video"
+            WHERE "thumbnailUrl" = 'https://vod-secure.twitch.tv/_404/404_processing_%{width}x%{height}.png'
+        `);
+
+        // Clear the 404 thumbnails
+        await queryRunner.query(`
+            UPDATE "video"
+            SET "thumbnailUrl" = NULL
+            WHERE "thumbnailUrl" = 'https://vod-secure.twitch.tv/_404/404_processing_%{width}x%{height}.png'
+        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        // Restore the old thumbnails
+        await queryRunner.query(`
+            UPDATE "video"
+            SET "thumbnailUrl" = video_1668442450700_removed_again."thumbnailUrl"
+            FROM video_1668442450700_removed_again
+            WHERE video.id = video_1668442450700_removed_again.id
+        `);
+
+        // Drop the backup
+        await queryRunner.query(`
+            DROP TABLE "video_1668442450700_removed_again";
+        `);
     }
 
 }
