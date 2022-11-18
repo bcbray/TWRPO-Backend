2970ed9d822c6946d1ebdcda4e8eea9cd5b13d7a
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-11-18 00:43:35 -0800

    Sam Winters is a Sheriff

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -2013,7 +2013,7 @@ export const wrpCharacters: WrpCharacters = {
     sgttyn: [
         { id: 723, name: 'Jacob Dubois' },
         { id: 724, name: 'Norris Olson' },
-        { id: 725, name: '[Deputy] Sam Winters', factions: ['Law'], telegram: 'DT600' },
+        { id: 725, name: '[Sheriff] Sam Winters', factions: ['Law'], telegram: 'DT600' },
     ],
     shadowsINsilence: [
         { id: 726, name: 'Emily Marie Kenward' },
