074de405782c8d35f48f0ca059c650a451ee2431
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-12-05 18:26:50 -0800

    Cuthbert Berry telegram

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -1204,7 +1204,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 1089, name: 'Rupert Romano' },
         { id: 1124, name: 'Dynamo Barbarosa', nicknames: ['The Doctor'] }, // Character last name Dolton but goes by Barbarosa
         { id: 1429, name: 'Edward Masters' },
-        { id: 1430, name: 'Cuthbert Berry' },
+        { id: 1430, name: 'Cuthbert Berry', telegram: 'BQ121' },
     ],
     IzzaBugg: [
         { id: 1184, name: '[Sheriff] Maisie Briar', factions: ['Law'], telegram: 'ZE633', deceased: true },
