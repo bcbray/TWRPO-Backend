698fa5bf2e5ea44b60ab6dc377fb34ce7f73193d
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-12-18 18:09:07 -0800

    New Del Lobos -> Del Lobos redirects

--- frontend/client/routes.tsx
+++ frontend/client/routes.tsx
@@ -51,6 +51,9 @@ export const redirects = <>
   <Route path="/streams/faction/theunhung" element={<Redirect to="/streams/faction/thebaastards" />} />
   <Route path="/characters/faction/theunhung" element={<Redirect to="/characters/faction/thebaastards" />} />
   <Route path="/multistream/faction/theunhung" element={<Redirect to="/multistream/faction/thebaastards" />} />
+  <Route path="/streams/faction/newdellobos" element={<Redirect to="/streams/faction/dellobos" />} />
+  <Route path="/characters/faction/newdellobos" element={<Redirect to="/characters/faction/dellobos" />} />
+  <Route path="/multistream/faction/newdellobos" element={<Redirect to="/multistream/faction/dellobos" />} />
   <Route path="/streams" element={<Redirect to="/" />} />
   <Route path="/streams/faction" element={<Redirect to="/" />} />
   <Route path="/streamer" element={<Redirect to="/" />} />
