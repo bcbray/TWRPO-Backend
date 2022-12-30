e2d0c90e3756f046afa94bfbeef38b1d59bcd282
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-12-29 21:56:28 -0800

    Forest is a Baastard

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -2148,7 +2148,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 1121, name: 'Kiono Kolichiyaw' },
     ],
     rossthehsauce: [
-        { id: 682, name: 'Murrietta Walton', nicknames: ['Forest', 'Forest Fish'], displayName: 3, factions: ['Guppy Gang'], telegram: 'IF559' },
+        { id: 682, name: 'Murrietta Walton', nicknames: ['Forest', 'Forest Fish'], displayName: 3, factions: ['The Baastards', 'Guppy Gang'], telegram: 'IF559' },
         { id: 683, name: 'Dominic "Dom" Disouza', factions: ['DiCenzo Famiglia'], telegram: 'HJ726' }, // Inactive DiCenzo
         { id: 684, name: 'Lionel Miles', factions: ['Miles Gang'], leader: true },
         { id: 1261, name: 'Jericho Gunne' },
