0a89a8696af03700041acb234ef55a5543f29f84
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-12-29 17:47:40 -0800

    Add Sheila O’Kelly

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -25,7 +25,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 1524
+// Next ID: 1525
 
 export const wrpCharacters: WrpCharacters = {
     '0Reed': [
@@ -2400,6 +2400,9 @@ export const wrpCharacters: WrpCharacters = {
         { id: 983, name: 'Malaha Mage' },
         { id: 1185, name: 'Monica McMonigal' },
         { id: 1416, name: 'Paddy Doyle', telegram: 'DS872' },
+    ],
+    squeakinghiccups: [
+        { id: 1524, name: 'Sheila O’Kelly' },
     ],
     Ssaab: [
         { id: 784, name: 'Sam Baas', factions: ['The Baastards'], leader: true, nicknames: ['Dank Outlaw'], assumeChar: true, deceased: true, telegram: 'YB638' }, // Former Sam's Club. leader of Sam's Club
