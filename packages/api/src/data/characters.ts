ec0cbda92e3fba9e06027ebf14fa4e0b6e7855a5
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-12-29 17:59:51 -0800

    Rook Pierce telegram

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -2432,7 +2432,7 @@ export const wrpCharacters: WrpCharacters = {
     ],
     stardaze: [
         { id: 789, name: 'Edie Barlowe' },
-        { id: 1462, name: 'Rook Pierce', factions: ['Red Water'] },
+        { id: 1462, name: 'Rook Pierce', factions: ['Red Water'], telegram: 'SM528' },
     ],
     start_vx: [
         { id: 1238, name: 'Garneesh Tandoori', factions: ['The Ring'], telegram: 'LL642' },
