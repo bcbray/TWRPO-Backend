09c584f45b3531706f636dbce0e83ea456b98804
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2022-12-29 18:08:01 -0800

    Andi Gotti is Andromeda

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -1775,7 +1775,7 @@ export const wrpCharacters: WrpCharacters = {
     MoogieCookies: [
         { id: 1230, name: 'Cecily Caldwell', telegram: 'AI841' },
         { id: 1319, name: 'Seraphine Nostradame' },
-        { id: 1523, name: 'Andi Gotti', factions: ['Quil Gang'] },
+        { id: 1523, name: 'Andromeda "Andi" Gotti', factions: ['Quil Gang'] },
     ],
     Mr_Jay_Gaming: [
         { id: 1331, name: 'Jay Walker' },
