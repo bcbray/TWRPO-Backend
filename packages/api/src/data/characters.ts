b6ff638c1a0fd99a9ae733f57eb204d5ff5dfa88
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-01-08 14:02:56 -0800

    Add Wendy Wallace

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -25,7 +25,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 1621
+// Next ID: 1622
 
 export const wrpCharacters: WrpCharacters = {
     '0porkchop0': [
@@ -2637,6 +2637,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 1092, name: 'Mercy Porter' },
         { id: 1111, name: '[Guard] Angela Payne', factions: ['Sisika Guard'] },
         { id: 1129, name: 'Jasmine Baro', factions: ['Independent', 'Guarma'] },
+        { id: 1621, name: 'Wendy Wallace' },
     ],
     Tech_Otter: [
         { id: 826, name: 'Chester McGuckin' },
