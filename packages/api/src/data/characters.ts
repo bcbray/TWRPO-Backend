dbc746c108b179d8ddf95ac69cac8eb67bd69c82
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-01-11 23:43:00 -0800

    Fix Al Timerson

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -976,7 +976,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 309, name: 'William Killmore' },
         { id: 1432, name: 'Augustus Hammerpenis' },
         { id: 1547, name: 'Sergei Clause' },
-        { id: 1548, name: 'Al Timeson' },
+        { id: 1548, name: 'Al Timerson' },
     ],
     FlawlessWhale: [
         { id: 1217, name: 'Michael "Big Mike" James' },
