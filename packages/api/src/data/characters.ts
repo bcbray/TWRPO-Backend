054205a3a1a404f5053c271d021d6ad241d895e9
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-01-11 23:55:56 -0800

    Add Delilah Tucker

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -25,7 +25,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 1644
+// Next ID: 1645
 
 export const wrpCharacters: WrpCharacters = {
     '0porkchop0': [
@@ -3013,6 +3013,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 1511, name: 'Ahnah Adjuk' },
         { id: 1642, name: 'Indigo Riley', telegram: 'XM689' },
         { id: 1643, name: 'Mirai Lee' },
+        { id: 1644, name: 'Delilah Tucker' },
     ],
     virtus: [
         { id: 1618, name: 'Dallas McKinney' },
