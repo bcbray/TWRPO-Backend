a1411bdbf71a71bd89aa3bd6339982f5e3c46e2e
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-01-16 15:02:52 -0800

    Lark Atwood is indeed a doctor

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -1700,7 +1700,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 1544, name: 'Hutch Vandal' },
     ],
     Lithiaris: [
-        { id: 518, name: '[Doctor?] Lark Atwood', factions: ['Medical'], telegram: 'MB861' },
+        { id: 518, name: '[Doctor] Lark Atwood', factions: ['Medical'], telegram: 'MB861' },
         { id: 519, name: '[Deputy] Sylvie Chevalier', factions: ['Law'], telegram: 'OR480' },
         { id: 1024, name: 'Hollyhock' },
     ],
