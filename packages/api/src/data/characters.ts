312eb6b7a3e5f69879c5798491178182cbd4916d
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-01-21 21:43:05 -0800

    Ludwig Weiß is a deputy

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -718,7 +718,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 1043, name: 'Avery Hobbs', displayName: 2, factions: ['The Black Company'] },
         { id: 1120, name: '[Guard] Cassius Renata', factions: ['Sisika Guard'] },
         { id: 1127, name: 'Sylas "Stache" Kristiansen', telegram: 'NO025' }, // Former Sheriff
-        { id: 1628, name: '[Cadet] Ludwig Weiß', nicknames: ['Ludwig Weiss'], factions: ['Law'] },
+        { id: 1628, name: '[Deputy] Ludwig Weiß', nicknames: ['Ludwig Weiss'], factions: ['Law'] },
     ],
     DeneeSays: [
         { id: 205, name: 'Brooke "Babs" Burns', factions: ['DiCenzo Famiglia'], telegram: 'AT704' },
