32dd68da7c4585b720ef23f7bf3b0968b16d4816
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-01-23 11:41:22 -0800

    Add Opal Foxx

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -25,7 +25,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 1727
+// Next ID: 1728
 
 export const wrpCharacters: WrpCharacters = {
     '0porkchop0': [
@@ -2618,6 +2618,7 @@ export const wrpCharacters: WrpCharacters = {
     Spoofey: [
         { id: 1354, name: 'Enola Quil', telegram: 'IX263', factions: ['Quil Gang'], deceased: true },
         { id: 1716, name: 'June Joy Lang' }, // Pretty sure about the last name, but not positive
+        { id: 1727, name: 'Opal Foxx' },
     ],
     sporkerific: [
         { id: 776, name: 'Rosamaria Sandoval' },
