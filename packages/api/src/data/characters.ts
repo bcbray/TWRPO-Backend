1ccdbf2a6c11fef06702c94d1270a329c1ad954e
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-01-24 16:32:53 -0800

    Chompy nickname

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -1985,7 +1985,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 584, name: 'Herman Wilbur' },
         { id: 585, name: 'Leon Taylor' },
         { id: 586, name: 'Conny Cage' },
-        { id: 1697, name: 'Enoch "Big Chomp" Holt', nicknames: ['Chomp'], telegram: 'JN479' },
+        { id: 1697, name: 'Enoch "Big Chomp" Holt', nicknames: ['Chomp', 'Chompy'], telegram: 'JN479' },
     ],
     Myre: [
         { id: 587, name: 'Benoit Baratie' },
