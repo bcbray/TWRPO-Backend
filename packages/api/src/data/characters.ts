815c026122c76159a405d8145fbee886a2d73799
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-01-24 18:09:23 -0800

    Cass Darling is deceased

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -1043,7 +1043,7 @@ export const wrpCharacters: WrpCharacters = {
     floralfoxes: [
         { id: 1350, name: '[Reporter] Juniper Ripley', nicknames: ['Juni', 'Juniper Lawrence'], telegram: 'IV378', deceased: true },
         { id: 1351, name: '[Ranger] Cat Carver', factions: ['Rangers'], telegram: 'IQ879' },
-        { id: 1545, name: 'Cass Darling' },
+        { id: 1545, name: 'Cass Darling', deceased: true },
         { id: 1546, name: 'Karina Durst' },
     ],
     Fluffalumpalump: [
