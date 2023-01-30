90486a1390b76f0297c8f2167a679d96b5e2975c
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-01-30 11:26:38 -0800

    Add Rosalinda Williams

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -25,7 +25,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 1770
+// Next ID: 1771
 
 export const wrpCharacters: WrpCharacters = {
     '0porkchop0': [
@@ -892,6 +892,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 263, name: '[Deputy] Eleonor Parker', factions: ['Law'], telegram: 'VQ924' },
         { id: 1543, name: 'Martha Dugmore' },
         { id: 1690, name: 'Ingrid Goldman', telegram: 'CV320' },
+        { id: 1770, name: 'Rosalinda Williams' },
     ],
     Eatindatcereal: [
         { id: 264, name: '[Sr. Deputy] Jax Sanctum', factions: ['Law'], telegram: 'OO930' },
