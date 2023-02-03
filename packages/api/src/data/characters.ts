c691deed23aa5b620b26af1814f1d1fe4534a6f6
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-02-02 17:17:09 -0800

    Add Dr. Kenzington Downey

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -25,7 +25,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 1778
+// Next ID: 1779
 
 export const wrpCharacters: WrpCharacters = {
     '0porkchop0': [
@@ -970,6 +970,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 1013, name: 'Annabelle Martin', deceased: true },
         { id: 1240, name: '[Ranger] Olya Ivanavia', factions: ['Rangers'], telegram: 'UB094' },
         { id: 1477, name: 'Revenant Wright', telegram: 'YB671' },
+        { id: 1778, name: '[Doctor] Kenzington Downey', factions: ['Medical'] },
     ],
     eternalsong: [
         { id: 285, name: 'Kora Vane', telegram: 'JH385' }, // Former Sam's Club
