3a94789b5fa7dc6614ba56b7e1a61994c24f8db0
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-02-09 08:34:58 -0800

    Add missing stream_chunk_stat index creation in migration

--- packages/api/scripts/migrations/1675134301116-StreamChunkStats.ts
+++ packages/api/scripts/migrations/1675134301116-StreamChunkStats.ts
@@ -6,9 +6,11 @@ export class StreamChunkStats1675134301116 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "stream_chunk_stat" ("id" SERIAL NOT NULL, "streamChunkId" integer NOT NULL, "time" TIMESTAMP NOT NULL, "viewerCount" integer NOT NULL, CONSTRAINT "PK_38796e255d429282cad22247854" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE INDEX "IDX_b829b50be53c209b695536de15" ON "stream_chunk_stat" ("streamChunkId") `);
+        await queryRunner.query(`CREATE INDEX "IDX_40dc6325fff47659ffc2b138ba" ON "stream_chunk_stat" USING BRIN ("time") WITH (pages_per_range = 70)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "public"."IDX_40dc6325fff47659ffc2b138ba"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_b829b50be53c209b695536de15"`);
         await queryRunner.query(`DROP TABLE "stream_chunk_stat"`);
     }
