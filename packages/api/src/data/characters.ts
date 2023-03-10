e141863ab53bcd9d9fcf393992b8786745ac3505
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-03-10 09:59:15 -0800

    Nate Casey is a deputy

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -3200,7 +3200,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 1868, name: 'Kaseem X' },
     ],
     ThePatrician69: [
-        { id: 875, name: '[Cadet] Nate Casey', factions: ['Law', 'Bluestone'], telegram: 'AV925' }, // Former Kelly Gang
+        { id: 875, name: '[Deputy] Nate Casey', factions: ['Law', 'Bluestone'], telegram: 'AV925' }, // Former Kelly Gang
         { id: 1680, name: 'Jesse Clay' },
     ],
     TheRandomChick: [
