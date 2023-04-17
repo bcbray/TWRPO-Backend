fe7cf7057ae7b2d7c736a27d51b4bbcf3b0d886b
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-04-17 12:03:37 -0700

    Add Emily Calloway

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -25,7 +25,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 2022
+// Next ID: 2023
 
 export const wrpCharacters: WrpCharacters = {
     '0porkchop0': [
@@ -818,6 +818,7 @@ export const wrpCharacters: WrpCharacters = {
     ],
     demolitiondan_: [
         { id: 1553, name: '[Deputy] Wallace Wilson', factions: ['Law'], telegram: 'CP350' },
+        { id: 2022, name: 'Emily Calloway' },
     ],
     DemonMungo98: [
         { id: 1899, name: 'No Knobbs' },
