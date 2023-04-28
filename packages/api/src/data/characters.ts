a6944f5dafffd4fce697760d7be9690ced91f0aa
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-04-27 17:22:00 -0700

    Alexio Gotti out of Quil Gang

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -1877,7 +1877,7 @@ export const wrpCharacters: WrpCharacters = {
     ],
     korean_jesus11: [
         { id: 1647, name: '[Deputy] Leon Buckley', factions: ['Law'] },
-        { id: 1648, name: 'Alexio Gotti', factions: ['Quil Gang'] },
+        { id: 1648, name: 'Alexio Gotti' }, // Former Quil Gang
     ],
     Kouts3: [
         { id: 479, name: 'Felix Colt' },
