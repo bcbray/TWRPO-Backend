6d60144e9944630204d1b40b65e4270c93d29db9
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-07-08 12:14:37 -0700

    Darren Mercer real name and Quil Gang

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -1045,7 +1045,7 @@ export const wrpCharacters: WrpCharacters = {
     elegendz77: [
         { id: 1403, name: '[Deputy] Terry Mallagher', factions: ['Law'], telegram: 'QW096' },
         { id: 1495, name: 'Toby Mallagher', deceased: true }, // Is this truly a different character?
-        { id: 1522, name: 'Darren The Drongo', displayName: 0 },
+        { id: 1522, name: 'Darren Mercer', nicknames: ['Darren The Drongo', 'Dazza'], displayName: 3, factions: ['Quil Gang'] },
         { id: 2084, name: 'Damien Cross' },
     ],
     EliDoesStuffIG: [
