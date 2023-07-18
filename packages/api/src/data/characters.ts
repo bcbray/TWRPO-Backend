c1f57c3b79e0eb247d27cc5a2f04bbc8c4693ba5
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-07-18 16:55:32 -0700

    Abraham Carter is a deputy

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -1595,7 +1595,7 @@ export const wrpCharacters: WrpCharacters = {
     im_unruly: [
         { id: 1425, name: 'Marquise "MF" Frazier', factions: ['Frazier Gang'], leader: true, telegram: 'TN764' },
         { id: 1685, name: 'Rudy "Rudeboy Rudy" Smalls', telegram: 'DF337' },
-        { id: 2074, name: '[Cadet] Abraham Carter', factions: ['Law'] },
+        { id: 2074, name: '[Deputy] Abraham Carter', factions: ['Law'] },
     ],
     inkyblackdreams: [
         { id: 410, name: 'Vera Addley', nicknames: ['Vera Dragavei'], factions: ['Little Gang'] },
