4eaf75aaf9d4051787f514ea1c9ff7f94018f776
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-07-18 17:01:09 -0700

    Add Matthew Klein

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -25,7 +25,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 2111
+// Next ID: 2112
 
 export const wrpCharacters: WrpCharacters = {
     '0porkchop0': [
@@ -1516,6 +1516,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 1424, name: 'Richard Gold', nicknames: ['Richie'], factions: ['Fuller House'], telegram: 'AH036' },
         { id: 1807, name: 'William Chillson' },
         { id: 2072, name: 'Steven Gaines' },
+        { id: 2111, name: 'Matthew Klein' },
     ],
     Hoss: [
         { id: 395, name: 'Buck Cherry' },
