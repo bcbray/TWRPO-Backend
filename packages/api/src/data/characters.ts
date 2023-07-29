ee8764ab79b4f0b2e8d4c72c338d6372db32ae20
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-07-29 11:08:00 -0700

    Add Oatis McQuakerson

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -25,7 +25,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 2163
+// Next ID: 2164
 
 export const wrpCharacters: WrpCharacters = {
     '0porkchop0': [
@@ -1994,6 +1994,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 1271, name: 'John Hancock', nicknames: ['Unnamed & Unattested', 'Terry Frost'], displayName: 0 },
         { id: 1333, name: '[Deputy] Jessie Valentine', telegram: 'XF491', factions: ['Law'] },
         { id: 1925, name: 'Victor Braithwaite' },
+        { id: 2163, name: 'Oatis McQuakerson' },
     ],
     KShorts: [
         { id: 2110, name: '[Cadet] Thomas Myles', factions: ['Law'] },
