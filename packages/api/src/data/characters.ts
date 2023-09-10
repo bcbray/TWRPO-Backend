23345407f218335fb20a875db80f0fbc835ea062
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-09-10 09:08:40 -0700

    Harley Bolton is a sheriff

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -4070,7 +4070,7 @@ export const wrpCharacters: WrpCharacters = {
     ],
     yeka221: [
         { id: 957, name: '[Sr. Ranger] Tabitha "Tibbit" Birch', nicknames: ['Tibbit Mostokova'], factions: ['Rangers'], telegram: 'RQ940' },
-        { id: 977, name: '[Undersheriff] Harley Bolton', factions: ['Law'], telegram: 'EA773' },
+        { id: 977, name: '[Sheriff] Harley Bolton', factions: ['Law'], telegram: 'EA773' },
         { id: 1790, name: 'Bonnie Jenkins' },
     ],
     Yorkoh: [
