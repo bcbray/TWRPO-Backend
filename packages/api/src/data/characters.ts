6bc03b047253a63cf54f094091a56bcf44a7f83f
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2023-12-24 19:15:38 -0800

    Sigynvina -> sigynvina

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -3180,7 +3180,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 734, name: 'Mr. White', displayName: 0, telegram: 'JJ244' }, // TODO: Don't know first name
         { id: 1334, name: 'Buck Curlay' },
     ],
-    Sigynvina: [
+    sigynvina: [
         { id: 1933, name: 'Rhea Williams' },
         { id: 2057, name: 'Julia Thomas' },
     ],
