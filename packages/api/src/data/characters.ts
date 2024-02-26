1e87d4cfd8289ac92a42be6ae447a7300f295d65
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2024-02-25 22:55:26 -0800

    Don Deters is a deputy

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -3897,7 +3897,7 @@ export const wrpCharacters: WrpCharacters = {
     topspin17: [
         { id: 1008, name: 'Archie Stonewall', telegram: 'BB139' },
         { id: 1060, name: 'Alvin Caster', factions: ['Dead End Gang'], telegram: 'GG577' },
-        { id: 1360, name: '[Cadet] Don Deters', telegram: 'AM073', factions: ['Law'] },
+        { id: 1360, name: '[Deputy] Don Deters', telegram: 'AM073', factions: ['Law'] },
         { id: 1561, name: 'Hershall Herbstreet' },
         { id: 1729, name: 'Chev Silver' },
         { id: 1795, name: 'Bryce Wilkins' },
