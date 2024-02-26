6db82e9049c26d435ef5a0a6e589507e7c11fed6
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2024-02-25 22:45:28 -0800

    VTM___ is playing on other servers

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -4040,7 +4040,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 1924, name: 'July Gray' },
     ],
     VTM___: [
-        { id: 925, name: 'Boone Morales', factions: ['Guarma'], telegram: 'JG239' }, // Guarma Guarda
+        { id: 925, name: 'Boone Morales', factions: ['Guarma'], telegram: 'JG239', assume: 'assumeOther' }, // Guarma Guarda
         { id: 926, name: 'Guy Hyneman' },
     ],
     vtrich: [
