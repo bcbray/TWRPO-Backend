e27a062fbac565b0e60487debf7c991251ec5e36
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2024-03-05 00:33:58 -0800

    RIP Benny Thatcher

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -2130,7 +2130,7 @@ export const wrpCharacters: WrpCharacters = {
     kono5alive: [
         { id: 1177, name: '[Warden] Lawrence "Big L" ?', factions: ['Sisika Guard'], deceased: true },
         { id: 1178, name: '[Deputy] Andrew Weaver', factions: ['Law'], deceased: true },
-        { id: 1292, name: 'Benjamin "Benny" Thatcher', formerFactions: ['Law'], telegram: 'LR040' }, // Former Deputy
+        { id: 1292, name: 'Benjamin "Benny" Thatcher', formerFactions: ['Law'], telegram: 'LR040', deceased: true }, // Former Deputy
         { id: 2186, name: 'Colson Steele' },
         { id: 2199, name: 'Diego ?', factions: ['Guarma'] },
     ],
