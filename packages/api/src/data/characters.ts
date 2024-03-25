a66d304e1dba209860670f349f3d17aa1ecd4026
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2024-03-25 14:55:41 -0700

    Add Johnny Silverstone

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -26,7 +26,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 2436
+// Next ID: 2437
 
 export const wrpCharacters: WrpCharacters = {
     '0porkchop0': [
@@ -1923,6 +1923,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 2144, name: 'Rufus Meecham' },
         { id: 2171, name: 'Tony James' },
         { id: 2399, name: 'River Rhodes' },
+        { id: 2436, name: 'Johnny Silverstone' },
     ],
     jakeyp0o: [
         { id: 432, name: 'Danner Wynn' },
