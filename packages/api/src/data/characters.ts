4f41276e10ffc68b6c3684abd94032afee873be5
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2024-04-02 12:14:18 -0700

    RIP David Caine

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -26,7 +26,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 2460
+// Next ID: 2461
 
 export const wrpCharacters: WrpCharacters = {
     '0porkchop0': [
@@ -412,6 +412,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 1929, name: 'Cassidy Gray', deceased: true },
         { id: 2059, name: 'Aoife Byrne' },
         { id: 2385, name: 'Lucas "Hush" Riley' },
+        { id: 2460, name: 'David Caine', deceased: true },
     ],
     BarryBogan: [
         { id: 72, name: 'Bernard "Bernie" Bogan', nicknames: ['Bernie the Butcher'] }, // Former Bloody Hood. Bloody Hoods disbanded
