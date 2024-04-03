1843c3f45b7b45d511088ff6d396eb6d80442094
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2024-04-03 13:12:22 -0700

    Add Terrance Witt

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -26,7 +26,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 2495
+// Next ID: 2496
 
 export const wrpCharacters: WrpCharacters = {
     '0porkchop0': [
@@ -976,6 +976,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 1141, name: 'Kit Bishop', telegram: 'OG840' },
         { id: 1340, name: 'Billy Burgs', telegram: 'TM375' },
         { id: 2494, name: 'Ezekiel Oatbottom' },
+        { id: 2495, name: 'Terrance Witt' },
     ],
     defanxious: [
         { id: 2015, name: 'William Soloch' },
