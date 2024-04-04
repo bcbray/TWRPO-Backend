89bc13ba41d14a099e84549e8352a7cff064864c
bcbray <brayyyyyyyyyyyyyyyy@gmail.com>
2024-04-03 21:46:23 -0700

    Add Sid Jones

--- packages/api/src/data/characters.ts
+++ packages/api/src/data/characters.ts
@@ -26,7 +26,7 @@ const reg = (r: RegExp): string => `/${r.source}/`;
 
 // Make character map
 
-// Next ID: 2556
+// Next ID: 2557
 
 export const wrpCharacters: WrpCharacters = {
     '0porkchop0': [
@@ -2032,6 +2032,7 @@ export const wrpCharacters: WrpCharacters = {
         { id: 2171, name: 'Tony James' },
         { id: 2399, name: 'River Rhodes' },
         { id: 2436, name: 'Johnny Silverstone' },
+        { id: 2556, name: 'Sid Jones' },
     ],
     jakeyp0o: [
         { id: 432, name: 'Danner Wynn' },
